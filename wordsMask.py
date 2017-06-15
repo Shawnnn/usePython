260898feed0cc9f283123d039d333518642d63e3
Shawnnn <429692326@qq.com>
2017-06-15 10:49:21 +0800

    Create wordsMask.py
    [rewrite  reference code](http://git.oschina.net/crossin/crossincode/blob/master/blocked_words/blocked_words.py)

--- wordsMask.py
+++ wordsMask.py
@@ -1,35 +1,65 @@
 #-*-coding:utf-8-*-
 #!/usr/bin/env python
 
-f=open('wordsList.txt','r')
-words=str(f.read())
-wordsList=words.split()
-print "wordList:"
-for word in wordsList:
-    print word
-print
+'''
+6/15/2017 shawn
+rewrite reference code 
+'''
+def load_blocked():
+    global words
+    with open("words.txt") as f:
+        words=f.readlines()
+        if not words:
+            print "empty file!"
+            return -1
+        words=[w.strip() for w in words if w]
+        print words
 
-# for i in f.readlines():
-#     print i
-test='从前明月光，疑似地上霜。\nShawn hello goodbye\nstudent HKU'
-raw_test=raw_input("please input test words: ")
-print raw_test
-if raw_test!='': #if raw_input only get enter pressed, then return none instead of '\n'
-    test=raw_test
+def text_blocked(text, charset="utf-8", symbol="*"):
+    for w in words:
+        text=text.replace(w, symbol*len(w.decode(charset)))#  attention: return a text
+    return text
 
-testList=test.split()
-print testList
+if __name__=='__main__':
+    load_blocked()
+    while True:
+        text=raw_input("please input text to be blocked, clicking enter directly to exit\n")
+        if not text:
+            break
+        print text_blocked(text)
+        
+'''
+my own incomplete version
+'''
+# f=open('wordsList.txt','r')
+# words=str(f.read())
+# wordsList=words.split()
+# print "wordList:"
+# for word in wordsList:
+#     print word
+# print
 
-print "testList:"
-for test in testList:
-    print test
-print
+# # for i in f.readlines():
+# #     print i
+# test='从前明月光，疑似地上霜。\nShawn hello goodbye\nstudent HKU'
+# raw_test=raw_input("please input test words: ")
+# print raw_test
+# if raw_test!='': #if raw_input only get enter pressed, then return none instead of '\n'
+#     test=raw_test
 
-for index,test in enumerate(testList):
-    for word in wordsList:
-        if test==word:
-            testList[index]="*"
-print "result:"
-for test in testList:
-    print test
+# testList=test.split()
+# print testList
+
+# print "testList:"
+# for test in testList:
+#     print test
+# print
+
+# for index,test in enumerate(testList):
+#     for word in wordsList:
+#         if test==word:
+#             testList[index]="*"
+# print "result:"
+# for test in testList:
+#     print test
 
